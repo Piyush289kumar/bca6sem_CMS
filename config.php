34a4ee4117e5d48fb2bdc1c2190bd63be2403836
Piyush289kumar <piyushraikwar289@gmail.com>
2024-05-28 23:18:59 +0530

    init

---
+++ config.php
@@ -0,0 +1,6 @@
+
+<?php
+$website_display_default_name = "CMS";
+$hostname = "http://localhost/CMS";
+$conn = mysqli_connect('localhost', 'root', '', 'cms_db') or die("Connection Failed!!" . mysqli_connect_error());
+?>
